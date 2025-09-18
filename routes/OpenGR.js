67646c837858d694a36aacc20aa545931f896810
France Salvador <france.salvador@roasters.com.ph>
2025-09-18 14:02:24 +0800

    Change OpenGR API endpoint from POST to GET for CSV processing

--- routes/OpenGR.js
+++ routes/OpenGR.js
@@ -4,8 +4,8 @@ import authenticateBearerToken from "../middleware/bearerTokenAuth.js";
 
 const router = express.Router();
 
-// POST endpoint to process CSV file from shared network path and save to database
-router.post(
+// GET endpoint to process CSV file from shared network path and save to database
+router.get(
   "/sap/opengr",
   authenticateBearerToken,
   openGRController.generateOpenGR
